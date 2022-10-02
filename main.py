62f06df9e3149a92bb29d7791797c2b6c95be227
Dooloppa2 <104138668+Dooloppa2@users.noreply.github.com>
2022-10-02 15:45:09 +0300

    Empty working window
    Just a simple window, that doesn't crush. Progress has been made.
    Also put the window parameters in a separate file for convenience

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Hello, World!')
+import pygame, sys
+from window_settings import *
+
+class Game:
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))  # Creating window
+        pygame.display.set_caption("Tutu")  # Naming a window
+        self.clock = pygame.time.Clock()  # Game timer for events
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():  # Getting events
+                if event.type == pygame.QUIT:  # Closing window when clicking a cross
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')  # Cleaning everything from screen by filling it black
+            pygame.display.update()  # Drawing everything
+            self.clock.tick(FPS)  # Doing a single tick
+
+
+if __name__ == '__main__':  # Work only when running as main
+    game = Game()
+    game.run()
